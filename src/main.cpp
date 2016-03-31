e59e8d6b7ebafb3ea6007f73955751fa3363f3c8
Andrew Wisner <awisner94@gmail.com>
2016-03-31 18:31:27 -0500

    Outlined order of execution and added some code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 /**
  * @author Samuel Andrew Wisner, awisner94@gmail.com
+ * @author Nicholas K. Nolan
  * @brief contains the main program
  */
 
+#include <array>
+#include <cstdlib>
 #include <iostream>
+#include <map>
+#include <pthread.h>
 #include <string>
+#include <unistd.h>
 
 #include "definitions.hpp"
-
 #include "fileio.hpp"
-#include "sigmath.hpp"
-#include "sound.hpp"
-#include "threadproc.hpp"
+#include "process.hpp"
 
 using namespace std;
 using namespace vaso;
@@ -21,5 +24,66 @@ using namespace vaso;
  * of days.
  */
 int main(int argc, char** argv) {
-	cout << "Aloha, World!" << endl;
+	// Constants declared
+	const uint8 REC_COUNT = 6;
+	const uint32 SAMPLE_COUNT = pow(2, 18);
+	const uint32 SAMPLE_FREQ = 48000;
+
+	// generate name for patient's file
+	string filename = "";//PatientName();
+
+	// TODO: Load all of patient's parameters
+
+	// Record doppler audio
+	uint8 counter = 0;  // number of current recording
+	float32** buffer;
+
+	for(uint8 i = 0; i < REC_COUNT; i++) {
+		buffer[i] = (float32*)malloc(SAMPLE_COUNT * sizeof(float32));
+	}
+	
+	ThreadParams p;
+	p.data = buffer;
+	p.recCount = REC_COUNT;
+	p.sampleCount = SAMPLE_COUNT;
+	p.sampleFreq = SAMPLE_FREQ;
+	p.counter = &counter;
+
+	pthread_t procThread;
+	int retThread = pthread_create(&procThread, NULL, Process, &p);
+
+	if(retThread != 0) {
+		cerr << "An error occurred creating the processing thread. "
+			"The program will now exit." << endl;
+		return ERROR;
+	}
+
+	for(uint8 i = 0; i < REC_COUNT; i++) {
+		// TODO: Prompt user to press ENTER to start recording
+
+		int retSeek = 0;//fseek(STDIN_FILENO, 0, SEEK_END);
+		int retRead = read(STDIN_FILENO, &buffer[i], SAMPLE_COUNT);
+
+		if(retSeek != 0 || retRead < SAMPLE_COUNT) {
+			cerr << "An error occurred reading the doppler audio! "
+				"The program will now exit." << endl;
+			return ERROR;
+		}
+
+		// TODO: Print message about recording stopped
+
+		counter++;
+	}
+
+	// wait for processing thread to finish, then get results
+	pthread_join(procThread, NULL);
+
+	// TODO: Print results & probable diagnosis
+
+	// TODO: Write all results to file
+	
+	// Free all malloc'd memory
+	for(uint8 i = 0; i < REC_COUNT; i++) {
+		free(buffer[i]);
+	}
 }
