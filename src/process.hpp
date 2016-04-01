4c16a072466483673ce79b64fd46c404f54a3397
Andrew Wisner <awisner94@gmail.com>
2016-03-31 19:00:40 -0500

    Made some changes...

--- src/process.hpp
+++ src/process.hpp
@@ -36,33 +36,82 @@ namespace vaso {
 	 * property of the aforementioned is protected and is neither misrepresented
 	 * nor claimed implicitly or explicitly by another individual. 
 	 *
+	 * @params data two-dimensional array (first dimension whole recordings,
+	 * second dimension samples in a recording) that will contain all recorded
+	 * audio
+	 *
+	 * @params recCount the number of recordings (left and right together) to be
+	 *  made
+	 *
+	 * @param sampleCount the number of samples in each recording. MUST be a
+	 * power of two.
+	 *
+	 * @param sampleFreq the sampling frequency in Hz or Samples/second
+	 *
 	 * @return a map of the averaged left- and right-side parameters in
 	 * DataParams structures
 	 */
-	void* Process(void* params) {
-		// redeclare structure
-		ThreadParams* p = (ThreadParams*)params;
+	std::map<Side, DataParams> Process(float32** data, uint8 recCount,
+			uint32 sampleCount, uint32 sampleFreq) {
 
-		// just in case p->sampleCount isn't a power of two
-		if((p->sampleCount & (p->sampleCount - 1) != 0) || p->sampleCount < 2) {
+		// just in case sampleCount isn't a power of two
+		if((sampleCount & (sampleCount - 1) != 0) || sampleCount < 2) {
 			throw std::invalid_argument(
 					"The number of samples is not a power of two!");
 		}
 
 		// declare function-scoped variables
-		uint32 freqSize = p->sampleCount / 2;
-		cfloat32 cdata[p->recCount][p->sampleCount];
-		float32 fdata[p->recCount][freqSize];
-		DataParams tempParams[p->recCount];
+		uint32 freqSize = sampleCount / 2;
+		cfloat32 cdata[recCount][sampleCount];
+		float32 fdata[recCount][freqSize];
+		DataParams tempParams[recCount];
+		std::map<Side, DataParams> sideParams;
 
-	
+		for(uint8 rCount = 0; rCount < recCount; rCount++) {
+			// convert data to complex numbers for fft()
+			for(uint32 i = 0; i < sampleCount; i++) {
+				cdata[rCount][i] = data[rCount][i];
+			}
+
+			// find frequency spectrum in relative decibels
+			fft(cdata[rCount], sampleCount);
+			mag(cdata[rCount], fdata[rCount], freqSize);
+			Maximum maximum = max(fdata[rCount], freqSize);
+
+			for(uint32 i = 0; i < freqSize; i++) {
+				fdata[rCount][i] /= maximum.value;
+			}
+
+			decibels(fdata[rCount], freqSize);
+			
+			/*
+			 * Run spectrum values through moving-average filter to smooth the
+			 * curve and make it easier to determine the derivative.
+			 */
+			smooth(fdata[rCount], freqSize, 20);
+
+			/*
+			 * Find the derivative of the smoothed spectrum. Bote that both this
+			 * filter and the previous are necessary to the algorithm.
+			 */
+			diff(fdata[rCount], freqSize);
+			smooth(fdata[rCount], freqSize, 100);
+			absolute(fdata[rCount], freqSize);
+
+			// find the parameters of this specific recording
+			uint16 offset = 1000;
+			uint32 index = max(absolute(fdata[rCount][offset],
+						freqSize - offset)).index;
+			tempParams[rCount].freq = index * (float)sampleFreq / freqSize;
+			tempParams[rCount].noise =
+				average(fdata[rCount][index + 2 * offset],
+						freqSize - 2 * offset);
+		}
 
 		// calculate the parameters for each side to be returned
-		std::map<Side, DataParams> sideParams;
-		p->results[Side::Left] = average(&tempParams[0], p->recCount / 2);
-		p->results[Side::Right] = average(&tempParams[p->recCount / 2],
-				p->recCount / 2);
-		return nullptr;
+		sideParams[Side::Left] = average(tempParams[0], rCount / 2);
+		sideParams[Side::Right] = average(tempParams[rCount / 2], rCount / 2);
+		return sideParams;
 	}
 }
 
