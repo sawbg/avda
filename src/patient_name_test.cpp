059f6a9b7765824fdc15a073ccd92b2f9a4464af
ZaykorVit <nicholasknolan@gmail.com>
2016-04-15 03:22:20 +0000

    Created these new files.

---
+++ src/patient_name_test.cpp
@@ -0,0 +1,23 @@
+/**
+ * @author Samuel Andrew Wisner, awisner94@gmail.com
+ * @author Nicholas K. Nolan
+ * @brief 
+ */
+
+#include <string>
+
+#include "definitions.hpp"
+#include "fileio.hpp"
+#include "process.hpp"
+
+using namespace std;
+using namespace vaso;
+
+/**
+ * 
+ * 
+ */
+int main(int argc, char** argv) {
+	string filename = PatientName();
+	cout << filename;
+}
---
+++ src/sigmath_test.cpp
@@ -0,0 +1,22 @@
+/**
+ * @author Samuel Andrew Wisner, awisner94@gmail.com
+ * @author Nicholas K. Nolan
+ * @brief 
+ */
+
+#include <string>
+
+#include "definitions.hpp"
+#include "fileio.hpp"
+#include "process.hpp"
+
+using namespace std;
+using namespace vaso;
+
+/**
+ * 
+ * 
+ */
+int main(int argc, char** argv) {
+
+}
