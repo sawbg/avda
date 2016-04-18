a52e935b51bb921c5cad850ab4f173b82aa11a60
Andrew Wisner <awisner94@gmail.com>
2016-04-18 22:26:49 +0000

    Got this to compile and run correctly.

--- src/process_test.cpp
+++ src/process_test.cpp
@@ -15,7 +15,7 @@
 #include "definitions.hpp"
 #include "process.hpp"
 
-#define COUNT 524228
+#define COUNT 131072
 
 using namespace std;
 using namespace vaso;
@@ -24,11 +24,25 @@ using namespace vaso;
  * 
  */
 int main(int argc, char** argv) {
-	int file = open("/home/pi/etc/audio/test.raw", O_RDONLY);
+	int file = open("/home/pi/vaso/etc/audio/test.raw", O_RDONLY);
+
+	if(file < 0) {
+		cerr << "File unreadable!" << endl;
+		return -1;
+	}
+
 	float32* buffer = (float32*)malloc(COUNT * sizeof(float32));
-	read(file, &buffer, COUNT);
+	int charRead = read(file, buffer, COUNT * sizeof(float32));
+
+	if(charRead < COUNT) {
+		cerr << "Too few bytes read!" << endl;
+		return -1;
+	}
+
 	close(file);
 
 	DataParams params = process(buffer, COUNT, SAMPLE_FREQ);
 	free(buffer);
+	cout << "Cutoff: " << params.freq << endl;
+	cout << "Noise: " << params.noise << endl;
 }
